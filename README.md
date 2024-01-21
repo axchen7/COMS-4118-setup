04d9ea1451da64dac591ffef51d316e17262fd02
Alex Chen <axchen7@outlook.com>
2024-01-21 17:00:01 -0500

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Columbia COMS 4118 Setup Script
+
+This repository contains a `setup.sh` script that brings a fresh Debian 11 installation up to speed for COMS 4118: Operating Systems.
+
+Run `su -` to become root, then run `./setup.sh`.
---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Check if running as root
+if [[ $EUID -ne 0 ]]; then
+    echo "Run `su -` to switch to root before running this script"
+    exit 1
+fi
+
+# add bullseye-backports source (for open-vm-tools arm64 on Debian 11)
+echo "deb http://deb.debian.org/debian/ bullseye-backports main" >> /etc/apt/sources.list
+
+# install packages
+apt update
+apt upgrade
+apt install -y sudo git build-essential net-tools linux-headers-$(uname -r)
+apt install -y vim
+apt install -y open-vm-tools
+
+# add user to sudo group
+echo "axchen7 ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+# add my MacBook's public key to authorized_keys
+mkdir -p /home/axchen7/.ssh
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOA5UHYRNLtR1n2llKMR9Dp8/ywG2FQk7Eu0R4oOAt3v axchen7@outlook.com" > /home/axchen7/.ssh/authorized_keys
+
+# edit git global config
+git config --global user.name "Alex Chen"
+git config --global user.email "azc2110@columbia.edu"
+
+# automatically mount shared folders
+mkdir -p /mnt/hgfs
+echo ".host:/ /mnt/hgfs fuse.vmhgfs-fuse allow_other 0 0" >> /etc/fstab
+mount -a
+
