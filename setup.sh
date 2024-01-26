14e706dc2116a66f4be1300d20caecf9a75ec278
Alex Chen <axchen7@outlook.com>
2024-01-25 20:17:55 -0500

    Add install VS code

--- setup.sh
+++ setup.sh
@@ -44,5 +44,16 @@ mkdir -p /mnt/hgfs
 echo ".host:/ /mnt/hgfs fuse.vmhgfs-fuse allow_other 0 0" >> /etc/fstab
 mount -a
 
+# install VS Code
+cd ~
+sudo apt install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
+
 # reboot to complete kedr install
 reboot
